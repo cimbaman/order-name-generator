26461d6bc75ff3272ab35a7ef040e5f50f0bd7d0
cimbaman <47815073+cimbaman@users.noreply.github.com>
2025-01-06 18:06:06 +0100

    Made so that there are 3 combinations

--- generator.js
+++ generator.js
@@ -13,6 +13,20 @@ export function generateName() {
 	const color = getRandomElement(colors);
 	const animal = getRandomElement(animals);
 
-	return `${adjective} ${color} ${animal}`;
+	// return `${adjective} ${color} ${animal}`;
+	const randomFormat = Math.random();
+
+	let name = "";
+
+	// Three possible formats
+	if (randomFormat < 0.5 ) {
+		name = `${color} ${animal}`;
+	} else if (randomFormat < 0.9 ) {
+		name = `${adjective} ${animal}`;
+	} else {
+		name = `${adjective} ${color} ${animal}`;
+	}
+
+	return name;
 }
 
