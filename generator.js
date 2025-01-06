e72bee27f485c9bee4c0f33b2d20a0427c09c505
cimbaman <47815073+cimbaman@users.noreply.github.com>
2025-01-06 15:02:52 +0100

    Initial setup

---
+++ generator.js
@@ -0,0 +1,17 @@
+function generateName() {
+    
+	const adjectives = ["Pure", "Pitch", "Brooding", "Mysterious", "Fierce", "Graceful"];
+	const colors = ["White", "Black", "Red", "Blue", "Peach", "Green"];
+	const animals = ["Elephant", "Hen", "Rabbit", "Lion", "Giraffe", "Panda"];
+
+	function getRandomElement(arr) {
+		const randomIndex = Math.floor(Math.random() * arr.length);
+		return arr[randomIndex];
+	}
+	
+	const adjective = getRandomElement(adjectives);
+	const color = getRandomElement(colors);
+	const animal = getRandomElement(animals);
+
+	return `${adjective} ${color} ${animal}`;
+}
